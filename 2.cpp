00da23004b569f6911fb0d2f4be1672c7ffbfac7
idiosun <sunh029@nenu.edu.cn>
2021-02-08 20:28:06 +0800

    csa

--- 2.cpp
+++ 2.cpp
@@ -1,31 +1,42 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 template<typename T>
 void selectionSort(T arr[],int n) {
-	for(int i=0;i<n;i++) {
-		//寻找[i,n)区间的最小值 
-		int minIndex = i; 
-		for(int j=i+1;j<n;j++) {
+	for(int i=0; i<n; i++) {
+		//寻找[i,n)区间的最小值
+		int minIndex = i;
+		for(int j=i+1; j<n; j++) {
 			if(arr[j]<arr[minIndex]) {
 				minIndex = j;
 			}
 			swap(arr[i],arr[minIndex]);
 		}
-		
-	} 
+
+	}
 }
 int main() {
-	int a[10]={10,9,8,7,6,5,4,3,2,1};
-		selectionSort(a,10);
-	for(int i=0;i<10;i++) {
-		cout<<a[i]<<" "; 
+	//int 类型数组
+	int a[10]= {10,9,8,7,6,5,4,3,2,1};
+	selectionSort(a,10);
+	for(int i=0; i<10; i++) {
+		cout<<a[i]<<" ";
 	}
 	cout<<endl;
-	double b[10]={10.3,9.6,3.9,7.9,6,5.8,4.9,3.5,2.4,1.3};
-		selectionSort(b,10);
-	for(int i=0;i<10;i++) {
-		cout<<b[i]<<" "; 
+
+	//double 类型数组
+	double b[10]= {10.3,9.6,3.9,7.9,6,5.8,4.9,3.5,2.4,1.3};
+	selectionSort(b,10);
+	for(int i=0; i<10; i++) {
+		cout<<b[i]<<" ";
+	}
+	cout<<endl;
+
+	//string 类型数组
+	string c[4]= {"c","v","d","e"};
+	selectionSort(c,4);
+	for(int i=0;i<4;i++) {
+		cout<<c[i]<<" ";
 	}
 	cout<<endl;
 	return 0;
-} 
+}
