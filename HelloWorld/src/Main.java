280baf2aa3afac207a068b04c14de845b2e29e21
idiosun <sunh029@nenu.edu.cn>
2021-02-09 15:53:29 +0800

    cds

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    private String name;
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        public int search() {
+            int a[] = {1, 2, 5, 7, 4, 9, 3, 0};
+            int s = 7;
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == s)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ LinearSearch/src/LinearSearch.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+    private LinearSearch() {};
+    public static <E> int search(E[] data,E target) {
+        for(int i=0;i<data.length;i++)
+            if(data[i].equals(target))
+                //判断类对象值相等
+                return i;
+        return -1;
+    }
+    public static void main(String[] args) {
+        Integer[] data = {89,0,67,46,36,27,78};
+        int res = LinearSearch.search(data,27);
+        System.out.println(res);
+        int res1 = LinearSearch.search(data,9);
+        System.out.println(res1);
+    }
+}
